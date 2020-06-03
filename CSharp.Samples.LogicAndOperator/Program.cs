c9c59ea60db9d2ea33ba4a4c46ea1e85f4e6fd03
Vadzim Papko <vadim.papko@gmail.com>
2020-06-03 20:35:53 +0300

    Added Logic And Operator Sample

---
+++ CSharp.Samples.LogicAndOperator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CSharp.Samples.LogicAndOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started at: " + DateTime.Now.TimeOfDay);
+
+            if (ReturnFalse() && ReturnTrue())
+            {
+                Console.WriteLine("I am there");
+            }
+
+            Console.WriteLine("Finished at: " + DateTime.Now.TimeOfDay);
+        }
+
+        static bool ReturnTrue()
+        {
+            Thread.Sleep(45_000);
+
+            return true;
+        }
+
+        static bool ReturnFalse()
+        {
+            Thread.Sleep(155_000);
+
+            Console.WriteLine("ReturnFalse method finished at: " + DateTime.Now.TimeOfDay);
+            return false;
+        }
+    }
+}
