dd72602d7350f6a7728bc4ada3e678efce35c1d9
Vadzim Papko <vadim.papko@gmail.com>
2020-11-16 18:34:28 +0300

    Added New Sample - Using array in Calc History

---
+++ CSharp.Samples.Array.Calculator/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Samples.Array.Calculator
+{
+    static class Calculator
+    {
+        static int _result;
+        static int[] _results = new int[5];
+
+        static int _operationNumber;
+
+
+        public static void Sum(int num1, int num2)
+        {
+            _result = num1 + num2;
+            SaveResult();
+            _operationNumber++;
+            Console.WriteLine(num1 + " + " + num2 + " = " + _result);
+        }
+
+        public static void Minus()
+        {
+            int numberA = 500;
+            int numberB = 300;
+
+            Console.WriteLine(numberA - numberB);
+        }
+
+        static void SaveResult()
+        {
+            _results[_operationNumber] = _result;
+        }
+
+        public static void ShowHistory()
+        {
+            Console.WriteLine("Your Calculator History:");
+
+            for (int i = 1; i <= _results.Length; i++)
+            {
+                Console.WriteLine(i + ": " + _results[i - 1]);   //fisrt time use _results[i] -> then [i-1]
+            }
+        }
+    }
+}
---
+++ CSharp.Samples.Array.Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp.Samples.Array.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            while (counter != 5)
+            {
+                Console.WriteLine("Input first number: ");
+                int number1 = Convert.ToInt32(Console.ReadLine()); counte
+
+                Console.WriteLine("Input second number: ");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                Calculator.Sum(number1, number2);
+                counter++;
+
+                Console.WriteLine();
+            }
+
+            Calculator.ShowHistory();
+        }
+    }
+}
