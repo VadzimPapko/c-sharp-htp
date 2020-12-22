1fc42b0fba59d12c9e3a5c88fb5e4b0c3cf92561
Vadzim Papko <v.papko@sam-solutions.com>
2020-12-22 13:07:07 +0300

    Added Serilog Console Sample

---
+++ CSharp.Samples.Serilog/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Serilog;
+
+namespace CSharp.Samples.Serilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Hour)
+                .CreateLogger();
+            
+            
+            Log.Information("Hello Logger");
+
+            int a = 10, b = 0;
+
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a,b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Something went wrong");
+            }
+        }
+    }
+}
