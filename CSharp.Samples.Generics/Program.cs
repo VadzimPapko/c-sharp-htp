ca00bd9e72a8a6204868580777abf5e11c831c25
Vadzim Papko <v.papko@sam-solutions.com>
2021-07-28 18:54:31 +0300

    Add sample with Dictionary

--- CSharp.Samples.Generics/Program.cs
+++ CSharp.Samples.Generics/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 namespace CSharp.Samples.Generics
 {
     class Program
     {
+        const string _pathToDumpFolder = @"C:\!academy\Demo";
         static void Main(string[] args)
         {
             //With int
@@ -32,17 +36,71 @@ namespace CSharp.Samples.Generics
             int number = moto.GetVin("99", 99);
 
             Dictionary<int, DateTime> pairs = new Dictionary<int, DateTime>();
-
-            //pairs.Add(99, DateTime.Now);
+            pairs.Add(99, DateTime.Now);
             //pairs.Add(99, DateTime.UtcNow);
-            //pairs[100] = DateTime.Today;
+            pairs[100] = DateTime.Today;
             pairs.TryGetValue(99, out DateTime value);
             var result = value;
 
-
             Hashtable table = new Hashtable();
-            pairs.Add(99, DateTime.Now);
-            pairs.Add(99, DateTime.UtcNow);
+            table.Add(99, DateTime.Now);
+            //table.Add(99, DateTime.UtcNow);
+
+            //Dictionary real Sample
+            var fileNames = GetFileNames();
+            var mapFiles = fileNames.Where(x => x.EndsWith("_map.json")).ToList();
+            var dictionary = GetMappedStructure(mapFiles);
         }
+
+        private static Dictionary<string, List<string>> GetMappedStructure(IEnumerable<string> fileNames)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            foreach (var fileName in fileNames)
+            {
+                var fileText = ReadFile(fileName);
+                var shortModel = JsonSerializer.Deserialize<ExhibitorDataModel>(fileText);
+                var dataSynqManufacturerID = shortModel.DataSynqManufacturerID;
+                var exhibitorIds = shortModel.ExhibitorId;
+
+                if (exhibitorIds == null || !exhibitorIds.Any())
+                {
+                    continue;
+                }
+
+                foreach (var exhibitorId in exhibitorIds)
+                {
+                    if (!result.ContainsKey(exhibitorId))
+                    {
+                        result.Add(exhibitorId, new List<string>());
+                    }
+
+                    result[exhibitorId].Add(dataSynqManufacturerID);
+                }
+            }
+
+            return result;
+        }
+
+        static IList<string> GetFileNames()
+        {
+            return Directory.GetFiles(_pathToDumpFolder).Select(Path.GetFileName).ToList();
+        }
+
+        static string ReadFile(string key)
+        {
+            if (!key.EndsWith(".json"))
+            {
+                key += ".json";
+            }
+
+            return File.ReadAllText(Path.Combine(_pathToDumpFolder, key));
+        }
+    }
+
+    public class ExhibitorDataModel
+    {
+        public string[] ExhibitorId { get; set; }
+        public string DataSynqManufacturerID { get; set; }
     }
 }
