324dc46aa0d02c8de65a6840af394353fc6ced26
Vadzim Papko <v.papko@sam-solutions.com>
2022-05-23 16:00:03 +0300

    Add Relative path sample

--- CSharp.Samples.IO/Program.cs
+++ CSharp.Samples.IO/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace CSharp.Samples.IO
 {
@@ -12,11 +13,17 @@ namespace CSharp.Samples.IO
 
         static void Main(string[] args)
         {
-            var files = GetFileNames();
+            // Relative Path Sample
+            //Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            //Console.WriteLine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            
+            //Relative Path Sample
+            //var files = Directory.GetFiles("../../../Data");
 
+            var files = GetFileNames();
             foreach (var file in files)
             {
-                if (File.Exists(Path.Combine(_pathToDumpFolder, file))) 
+                if (File.Exists(Path.Combine(_pathToDumpFolder, file)))
                 {
                     string content = ReadFile(file);
                     Console.WriteLine(content);
@@ -24,13 +31,15 @@ namespace CSharp.Samples.IO
                     try
                     {
                         Data data = JsonConvert.DeserializeObject<Data>(content);
+                        //var json = JsonConvert.SerializeObject(data);
+                        //WriteFile(json);
                     }
                     catch (JsonSerializationException ex)
                     {
                         Console.WriteLine(ex.Message);
                         Console.WriteLine(ex.StackTrace);
                     }
-                    catch (JsonReaderException ex) 
+                    catch (JsonReaderException ex)
                     {
                         Console.WriteLine(ex.Message);
                         Console.WriteLine(ex.StackTrace);
@@ -47,6 +56,11 @@ namespace CSharp.Samples.IO
         static string ReadFile(string fileName) 
         {
             return File.ReadAllText(Path.Combine(_pathToDumpFolder, fileName));
+        }
+
+        static void WriteFile(string text) 
+        {
+            File.WriteAllText(Path.Combine(_pathToDumpFolder, "new.json"), text);
         }
     }
 }
